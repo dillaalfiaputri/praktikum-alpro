f4927c4f345dbd5f8caeada168980a79aeb82ff6
via <dillaalfiaputri@gmail.com>
2025-04-14 11:03:17 +0700

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,12 @@
+# Contoh 1
+print('FOR IN SEQUENCE')
+nama = ['Alfy' , 'Alif' , 'Danu' , 'Kiki' , 'Maira' ]
+for data in nama :
+    print('Nama :',data)
+print('----------------')
+
+# Contoh 2
+print('FOR IN RANGE')
+for nilai in range(1,11) :
+    print('Pengulangan ke :',nilai)
+print('-----------------')
---
+++ whileloop.py
@@ -0,0 +1,9 @@
+# print('Pengulangan ke : 1')
+# print('Pengulangan ke : 2')
+# print('Pengulangan ke : 3')
+
+nilaiAwal = 1
+while nilaiAwal <= 1000 :
+    print('Pengulangan ke :',nilaiAwal)
+    nilaiAwal += 1
+    # nilaiAwal = nilaiAwal + 1
