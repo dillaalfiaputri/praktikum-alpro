282943bf4ee2e4105c8567f70a6d0ff6cd599f65
via <dillaalfiaputri@gmail.com>
2025-04-23 10:08:48 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+# buat function
+def sapa() :
+    print('Halo')
+    print('Selamat Belajar')
+    print('Bahasa Pemrograman Python')
+    print('--------------------------')
+
+# panggil function
+sapa()
